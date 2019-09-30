b551466bd1e314f573b4aba6e6fc7e1b81c1b52b
Marvin Dawson <marvintdawson@gmail.com>
2019-09-29 20:46:18 -0500

    added inline styling

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -11,16 +11,16 @@ class Counter extends Component {
     }
 
     render() {
+        const style = {fontSize: "1.6rem", padding: "1.5rem 3rem"};
         return (
             <div className="counter">
                 <button onClick={this.increment}>+1</button>
-                <span className="count">{this.state.counter}</span>
+                <span className="count" style={style}>{this.state.counter}</span>
             </div>
         )
-    }
+    };
 
-    increment(){
-    // console.log("Button was clicked");
+    increment (){
     this.setState({
         counter: this.state.counter + 1
     });
